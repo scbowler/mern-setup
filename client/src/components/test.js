6154db291965b914880497d960843f8b8910938a
Scott Bowler <scott.bowler@learningfuze.com>
2018-05-10 17:45:49 -0700

    Setup for deployment

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -5,10 +5,30 @@ class Test extends Component {
 
     componentDidMount(){
         this.getUserData();
+        this.getArticle();
+        this.sendData();
+    }
+
+    async sendData(){
+        const dataToSend = {
+            something: 'Here is something',
+            anotherThing: 'Here is another thing',
+            stuff: 'What is all this then?'
+        };
+
+        const response = await axios.post('/api/send-data', dataToSend);
+
+        console.log('Send Data:', response);
+    }
+
+    async getArticle(){
+        const response = await axios.get('/api/get-article');
+
+        console.log('Article Data:', response);
     }
 
     async getUserData(){
-        const response = await axios.get('http://localhost:9000/user-data');
+        const response = await axios.get('/api/user-data');
 
         console.log('User Data Response:', response);
     }
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { resolve } = require('path');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
-app.get('/user-data', (req, res) => {
+app.post('/api/send-data', (req, res) => {
+    console.log('Data sent:', req.body);
+
+    res.send({success: true, mirror: req.body});
+});
+
+app.get('/api/user-data', (req, res) => {
     const user = {
         name: 'Jim Bob',
         email: 'jimthebob@mail.com',
@@ -15,8 +24,18 @@ app.get('/user-data', (req, res) => {
     res.send(user);
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Something else!!!!</h1>');
+app.get('/api/get-article', (req, res) => {
+    const article = {
+        title: 'Some Awesome Article',
+        author: 'John Walker',
+        content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, vel vero? Quas expedita tempore, suscipit iusto repellendus aliquid animi corrupti?'
+    };
+
+    res.send(article);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
